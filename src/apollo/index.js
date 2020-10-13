35e6e39987ebb2d579db9aa582736533db15a297
angeldesweb <angeldesweb@gmail.com>
2020-10-13 03:02:32 -0400

    LIMPIEZA

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -5,8 +5,7 @@ let base_url = ''
 
 if(process.env.NODE_ENV === 'development') base_url = process.env.REACT_APP_API_URL;
 
-if(process.env.NODE_ENV === 'production') base_url = process.env.API_URL
-
+if(process.env.NODE_ENV === 'production') base_url = 'https://cne-notas-api.herokuapp.com'
 
 const link = createUploadLink({
   uri:`${base_url}/graphql`,
